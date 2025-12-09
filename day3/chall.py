44b283aefe2a0d1045337f2eeea1246fd591c581
luca-simei <luca.simei@mail.polimi.it>
2025-12-09 12:14:44 -0800

    working on day 3

---
+++ day3/chall.py
@@ -0,0 +1,26 @@
+def solve1(input_data):
+    result = 0
+    lines = input_data.splitlines()
+    for line in lines:
+        first_value = 0
+        first_idx = 0
+        for i in range(len(line)-1):
+            if first_value < int(line[i]):
+                first_value = int(line[i])
+                first_idx = i
+        second_value = 0
+        for i in range(first_idx+1, len(line)):
+            if second_value < int(line[i]):
+                second_value = int(line[i])
+        result += int(str(first_value) + str(second_value))
+        with open("output1.txt", "a") as f:
+            f.write(f"{first_value} {second_value} -> {result}\n")
+
+    return result
+
+
+
+if __name__ == "__main__":
+    with open("input.txt") as f:
+        input_data = f.read().strip()
+        print(solve1(input_data))

6eaeaffbc052fad0c2464f6642b0420973139daa
luca-simei <luca.simei@mail.polimi.it>
2025-12-09 12:38:35 -0800

    day 3 completed

--- day3/chall.py
+++ day3/chall.py
@@ -19,8 +19,26 @@ def solve1(input_data):
     return result
 
 
+def solve2(input_data):
+    result = 0
+    lines = input_data.splitlines()
+    for line in lines:
+        joltage = []
+        start_idx = 0
+        for j in range(12):
+            joltage.append(0)
+            for i in range(start_idx, len(line)-11+j):
+                if joltage[j] < int(line[i]):
+                    joltage[j] = int(line[i])
+                    start_idx = i + 1
+        result += int(''.join(str(x) for x in joltage))
+        with open("output2.txt", "a") as f:
+            f.write(f"{joltage} -> {result}\n")
+    return result
+        
 
 if __name__ == "__main__":
     with open("input.txt") as f:
         input_data = f.read().strip()
         print(solve1(input_data))
+        print(solve2(input_data))
